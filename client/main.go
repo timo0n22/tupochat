83000b4acc70150ac9d0ea7215e56cfa4edfccf2
loginovartem <akkdlyaandroid@gmail.com>
2025-05-19 21:42:17 +0600

    goroutine for messages loop

--- client/main.go
+++ client/main.go
@@ -1,15 +1,22 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"net"
+	"os"
 )
 
 func main() {
 	conn, _ := net.Dial("tcp", "localhost:9999")
-	conn.Write([]byte("let's talk?"))
-	buf := make([]byte, 1024)
-	conn.Read(buf)
-	fmt.Println(string(buf))
+	var text string
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		fmt.Print("message: ")
+		text, _ = reader.ReadString('\n')
+		if text == "/exit" {
+			break
+		}
+		conn.Write([]byte(text))
+	}
 }
-
--- main.go
+++ main.go
@@ -1,15 +1,29 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"net"
 )
 
+func HandleConnection(conn net.Conn) {
+	defer conn.Close()
+	reader := bufio.NewReader(conn)
+	for {
+		n, _ := reader.ReadString('\n')
+		if n == "/exit\n" {
+			fmt.Println("client exit")
+			break
+		}
+		fmt.Println("client msg:", n)
+	}
+}
+
 func main() {
 	ln, _ := net.Listen("tcp", ":9999")
-	conn, _ := ln.Accept()
-	buf := make([]byte, 1024)
-	n, _ := conn.Read(buf)
-	fmt.Println("Получено:", string(buf[:n]))
-	conn.Write([]byte("no"))
+	for {
+		conn, _ := ln.Accept()
+		go HandleConnection(conn)
+	}
+	fmt.Println("server exit")
 }
