935949c8947b8606010d0ad4a903b5749170835f
loginovartem <akkdlyaandroid@gmail.com>
2025-09-05 22:38:59 +0600

    moooore fixes for damn db

--- tupochat_schema.sql
+++ tupochat_schema.sql
@@ -1,21 +1,23 @@
-CREATE TABLE clients (
-    username VARCHAR(20) UNIQUE NOT NULL,
-    display_name VARCHAR(20),
-    password_hash VARCHAR(300),
-    current_room VARCHAR(20) REFERENCES rooms (name) ON DELETE SET NULL
-);
-
-CREATE TABLE messages (
-    sender VARCHAR(20) REFERENCES clients (username) ON DELETE SET NULL,
-    content VARCHAR(1000),
-    sent_at VARCHAR(20),
-    room VARCHAR(20) REFERENCES rooms (name) ON DELETE SET NULL
-);
-
 CREATE TABLE rooms (
     name VARCHAR(20) UNIQUE NOT NULL,
     owner VARCHAR(20)
 );
 
-INSERT INTO clients (username, display_name, password_hash, current_room) VALUES ('server', 'server', 'server', 'global');
-INSERT INTO rooms (name, owner) VALUES ('global', 'server');
+CREATE TABLE clients (
+    username VARCHAR(20) UNIQUE NOT NULL,
+    display_name VARCHAR(20),
+    password_hash VARCHAR(300),
+    current_room VARCHAR(20) REFERENCES rooms(name) ON DELETE SET NULL
+);
+
+CREATE TABLE messages (
+    sender VARCHAR(20) REFERENCES clients(username) ON DELETE SET NULL,
+    content VARCHAR(1000),
+    sent_at VARCHAR(20),
+    room VARCHAR(20) REFERENCES rooms(name) ON DELETE SET NULL
+);
+
+INSERT INTO rooms (name, owner) VALUES ('global', NULL);
+
+INSERT INTO clients (username, display_name, password_hash, current_room)
+VALUES ('server', 'server', 'server', 'global');
